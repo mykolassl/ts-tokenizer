fbe96683b63c9debdbfdbf7638cc25f9e83d8f33
mykolassl <emykolas@gmail.com>
2023-09-26 11:10:30 +0300

    add base cases for tokenizer

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-const TokenType = {
-    Illegal: "ILLEGAL",
+const TokenTypes = {
     Eof: "EOF",
-    Ident: "IDENT",
     If: "if",
     Return: "return",
     True: "true",
@@ -21,17 +19,25 @@ const TokenType = {
     Function: "FUNCTION",
     Let: "LET",
     Bang: "!",
-    Dash: "-",
-    ForwardSlash: "/",
+    Minus: "-",
+    Slash: "/",
     Asterisk: "*",
-    LessThan: "<",
-    GreaterThan: ">",
+    LT: "<",
+    GT: ">",
+    Identifier: "IDENT",
+    Illegal: "ILLEGAL",
 } as const;
+
+type TokenType = (typeof TokenTypes)[keyof typeof TokenTypes];
 
 type Token = {
     value: string;
-    type: (typeof TokenType)[keyof typeof TokenType];
+    type: TokenType;
 };
+
+function createToken(value: string, type: TokenType): Token {
+    return { value, type };
+}
 
 class Tokenizer {
     private position = 0;
@@ -49,6 +55,84 @@ class Tokenizer {
         });
     }
 
+    nextToken(): Token | undefined {
+        this.skipWhitespace();
+        let token: Token | undefined;
+
+        switch (this.ch) {
+            case "\0":
+                token = createToken(this.ch, TokenTypes.Eof);
+                break;
+            case "+":
+                token = createToken(this.ch, TokenTypes.Plus);
+                break;
+            case "-":
+                token = createToken(this.ch, TokenTypes.Minus);
+                break;
+            case "*":
+                token = createToken(this.ch, TokenTypes.Asterisk);
+                break;
+            case "/":
+                token = createToken(this.ch, TokenTypes.Slash);
+                break;
+            case ",":
+                token = createToken(this.ch, TokenTypes.Comma);
+                break;
+            case "<":
+                token = createToken(this.ch, TokenTypes.LT);
+                break;
+            case ">":
+                token = createToken(this.ch, TokenTypes.GT);
+                break;
+            case ";":
+                token = createToken(this.ch, TokenTypes.Semicolon);
+                break;
+            case "(":
+                token = createToken(this.ch, TokenTypes.LParen);
+                break;
+            case ")":
+                token = createToken(this.ch, TokenTypes.RParen);
+                break;
+            case "{":
+                token = createToken(this.ch, TokenTypes.LSquirly);
+                break;
+            case "}":
+                token = createToken(this.ch, TokenTypes.RSquirly);
+                break;
+            case "!":
+                if (this.peekChar() === "=") {
+                    this.readChar();
+                    token = createToken("!=", TokenTypes.NotEqual);
+                } else {
+                    token = createToken(this.ch, TokenTypes.Bang);
+                }
+
+                break;
+            case "=":
+                if (this.peekChar() === "=") {
+                    this.readChar();
+                    token = createToken("==", TokenTypes.Equal);
+                } else {
+                    token = createToken(this.ch, TokenTypes.Assign);
+                }
+
+                break;
+        }
+
+        return token;
+    }
+
+    private skipWhitespace(): void {
+        while (
+            this.ch === " " ||
+            this.ch === "\t" ||
+            this.ch === "\r" ||
+            this.ch === "\n"
+        ) {
+            this.readChar();
+        }
+    }
+
     private readChar() {
         if (this.peekPosition >= this.input.length) {
             this.ch = "\0";
@@ -59,7 +143,23 @@ class Tokenizer {
         this.position = this.peekPosition;
         this.peekPosition++;
     }
+
+    private peekChar(): string {
+        if (this.peekPosition >= this.input.length) {
+            return "\0";
+        } else {
+            return this.input[this.peekPosition];
+        }
+    }
 }
 
 const text = "let x = 5;";
 const tokenizer = new Tokenizer(text);
+
+while (true) {
+    const token = tokenizer.nextToken();
+
+    if (!token) break;
+
+    console.log(token);
+}
